8478e91fd72bba76ebfa412d3d0a427eeafe7684
Kira Ablamunits <kira.abl@gmail.com>
2020-10-07 23:15:33 +0300

    add class exercise

--- Python.py
+++ Python.py
@@ -12,3 +12,22 @@ def hiYou(name):
 hiYou("John")
 hiYou("James")
 hiYou("Maggie")
+
+def hiMe(name="sir"):
+    print("Hello " + name + "! How are you today?")
+
+hiMe()
+hiMe("James")
+
+class Car:
+
+    def __init__(self, mod, max_speed):
+        self.model = mod
+        self.max_speed = max_speed
+        self.speed = 0
+
+bmw_x6 = Car(mod="BMW X6", max_speed=230)
+
+print(bmw_x6.model)
+print(bmw_x6.max_speed)
+print(bmw_x6.speed)
