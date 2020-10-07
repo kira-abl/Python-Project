e2535631bedfd03877e1f7b8a35668cfa3b4b2a7
Kira Ablamunits <kira.abl@gmail.com>
2020-10-07 22:47:37 +0300

    add a new function to tutorial

---
+++ Python.py
@@ -0,0 +1,13 @@
+def hi():
+    print("Hello Mr. Jones! How are you today?")
+
+hi()
+hi()
+hi()
+
+def hiYou(name):
+    print("Hello " + name + "! How are you today?")
+
+hiYou("John")
+hiYou("James")
+hiYou("Maggie")
